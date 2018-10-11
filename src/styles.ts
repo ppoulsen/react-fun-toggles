293003ea5e6cc06b5110344891da0878ea592ba6
Paul Poulsen <ppoulsen21@gmail.com>
2018-10-11 09:39:03 -0500

    Express sizes as a factor of height

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,35 @@
+import { color, px } from 'csx';
 import { style } from 'typestyle';
+
+// Sizes & Positions
+const HEIGHT_PX = 30;
+const BORDER_PX = Math.round(HEIGHT_PX * (5 / 70));
+const BORDER_PADDING_PX = Math.round(HEIGHT_PX * (2 / 70));
+const WIDTH_PX = Math.round(HEIGHT_PX * (125 / 70));
+const TRANSLATE_SHIFT_PX = Math.round(HEIGHT_PX * (51 / 70));
+const TRANSLATE_FACE_SHIFT_PX = Math.round(HEIGHT_PX * (53 / 70));
+const FACE_HEIGHT_PX = Math.round(HEIGHT_PX * (56 / 70));
+const EYE_HEIGHT_PX = Math.round(HEIGHT_PX / 10);
+const EYE_LEFT_OFFSET_PX = Math.round(HEIGHT_PX * (12 / 70));
+const EYE_SPACE_BETWEEN_PX = Math.round(HEIGHT_PX * (28 / 70));
+const INNER_MOUTH_HEIGHT_PX = EYE_HEIGHT_PX;
+const INNER_MOUTH_WIDTH_PX = Math.round(HEIGHT_PX * (17 / 70));
+const INNER_MOUTH_LEFT_OFFSET_PX = Math.round(HEIGHT_PX * (9 / 70));
+const INNER_MOUTH_TOP_OFFSET_PX = Math.round(HEIGHT_PX * (21 / 70));
+const OUTER_MOUTH_HEIGHT_PX = INNER_MOUTH_WIDTH_PX;
+const OUTER_MOUTH_WIDTH_PX = Math.round(HEIGHT_PX * (36 / 70));
+const OUTER_MOUTH_LEFT_OFFSET_PX = 0;
+const OUTER_MOUTH_TOP_OFFSET_PX = Math.round(HEIGHT_PX * (11 / 70));
+
+// Colors
+const UNCHECKED_COLOR = '#E25D5D';
+const UNCHECKED_COLOR_BORDER = color(UNCHECKED_COLOR)
+  .darken(0.25)
+  .toHexString();
+const CHECKED_COLOR = '#3498DB';
+const CHECKED_COLOR_BORDER = color(CHECKED_COLOR)
+  .darken(0.25)
+  .toHexString();
 
 const transition = {
   transition: 'all 250ms ease-in',
@@ -32,8 +63,8 @@ export const toggleCheckBox = style(transition, {
 export const toggleBtn = style(transition, {
   $nest: {
     [`.${toggleCheckBox}:checked + &`]: {
-      backgroundColor: '#3498DB',
-      border: '5px solid #0865B0',
+      backgroundColor: CHECKED_COLOR,
+      border: `${px(BORDER_PX)} solid ${CHECKED_COLOR_BORDER}`,
     },
     '&::after': {
       ...beforeAfterShared,
@@ -41,39 +72,37 @@ export const toggleBtn = style(transition, {
     '&::before': {
       ...beforeAfterShared,
       [`.${toggleCheckBox}:checked + &`]: {
-        transform: 'translate(51px, 0)',
+        transform: `translate(${px(TRANSLATE_SHIFT_PX)}, 0)`,
       },
       backgroundColor: '#F2DD68',
-      border: '5px solid #E5CE5E',
+      border: `${px(BORDER_PX)} solid #E5CE5E`,
       borderRadius: '50%',
       boxSizing: 'border-box',
-      height: '56px',
-      left: '4px',
+      height: px(FACE_HEIGHT_PX),
+      left: px(BORDER_PADDING_PX),
       position: 'absolute',
-      top: '2px',
-      width: '56px',
+      top: px(BORDER_PADDING_PX),
+      width: px(FACE_HEIGHT_PX),
     },
     '&:hover': {
       cursor: 'pointer',
     },
   },
-  backgroundColor: '#E25D5D',
-  border: '5px solid #AD1000',
-  borderRadius: '70px',
+  backgroundColor: UNCHECKED_COLOR,
+  border: `${px(BORDER_PX)} solid ${UNCHECKED_COLOR_BORDER}`, // #AD1000
+  borderRadius: px(HEIGHT_PX),
   boxSizing: 'border-box',
   display: 'block',
-  fontSize: '1.4em',
-  height: '70px',
-  margin: '0 auto',
+  height: px(HEIGHT_PX),
   position: 'relative',
   transition: 'all 350ms ease-in',
-  width: '125px',
+  width: px(WIDTH_PX),
 });
 
 export const toggleFeature = style(transition, {
   $nest: {
     [`.${toggleCheckBox}:checked + .${toggleBtn} &`]: {
-      transform: 'translate(53px, 0)',
+      transform: `translate(${px(TRANSLATE_FACE_SHIFT_PX)}, 0)`,
     },
     '&::after': {
       ...beforeAfterShared,
@@ -81,33 +110,39 @@ export const toggleFeature = style(transition, {
         borderRadius: '50%/50%',
       },
       backgroundColor: '#D8AA2B',
-      borderRadius: '17px 17px 0 0',
-      height: '7px',
-      left: '9px',
+      borderRadius: `${px(INNER_MOUTH_WIDTH_PX)} ${px(
+        INNER_MOUTH_WIDTH_PX
+      )} 0 0`,
+      height: px(INNER_MOUTH_HEIGHT_PX),
+      left: px(INNER_MOUTH_LEFT_OFFSET_PX),
       position: 'absolute',
-      top: '21px',
-      width: '17px',
+      top: px(INNER_MOUTH_TOP_OFFSET_PX),
+      width: px(INNER_MOUTH_WIDTH_PX),
     },
     '&::before': {
       ...beforeAfterShared,
       [`.${toggleCheckBox}:checked + .${toggleBtn} &`]: {
-        borderRadius: '0 0 36px 36px',
+        borderRadius: `0 0 ${px(OUTER_MOUTH_WIDTH_PX)} ${px(
+          OUTER_MOUTH_WIDTH_PX
+        )}`,
       },
       backgroundColor: '#995710',
-      borderRadius: '36px 36px 0 0',
-      height: '17px',
-      left: '0',
+      borderRadius: `${px(OUTER_MOUTH_WIDTH_PX)} ${px(
+        OUTER_MOUTH_WIDTH_PX
+      )} 0 0`,
+      height: px(OUTER_MOUTH_HEIGHT_PX),
+      left: px(OUTER_MOUTH_LEFT_OFFSET_PX),
       position: 'absolute',
-      top: '11px',
-      width: '36px',
+      top: px(OUTER_MOUTH_TOP_OFFSET_PX),
+      width: px(OUTER_MOUTH_WIDTH_PX),
     },
   },
   backgroundColor: '#995710',
   borderRadius: '50%',
-  boxShadow: '28px 0 0 0 #995710',
-  height: '7px',
-  left: '14px',
+  boxShadow: `${px(EYE_SPACE_BETWEEN_PX)} 0 0 0 #995710`,
+  height: px(EYE_HEIGHT_PX),
+  left: px(EYE_LEFT_OFFSET_PX),
   position: 'absolute',
   top: '32%',
-  width: '7px',
+  width: px(EYE_HEIGHT_PX),
 });
