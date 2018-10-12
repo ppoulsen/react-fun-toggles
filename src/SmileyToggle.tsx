b52ffde8bbafe02ba27171aa3ba6c9424bd3cefb
Paul Poulsen <ppoulsen21@gmail.com>
2018-10-12 10:48:17 -0500

    Controlled and uncontrolled versions

--- src/SmileyToggle.tsx
+++ src/SmileyToggle.tsx
@@ -4,35 +4,69 @@ import * as styles from './styles';
 
 interface ISmileyToggleProps {
   checked?: boolean;
-  onClick?: React.MouseEventHandler<HTMLElement>;
+  defaultChecked?: boolean;
+  onChange?: (checked: boolean) => void;
 }
 
 let counter = 0;
 export default class SmileyToggle extends React.PureComponent<
-  ISmileyToggleProps,
-  any,
-  any
+  ISmileyToggleProps
 > {
-  public static defaultProps = {
-    checked: false,
-  };
-
   public id: string;
 
-  constructor(props: ISmileyToggleProps, context?: any) {
-    super(props, context);
+  constructor(props: ISmileyToggleProps) {
+    super(props);
     this.id = 'toggle' + counter;
     counter++;
   }
 
   public render() {
+    const conditionalInputProps: React.InputHTMLAttributes<
+      HTMLInputElement
+    > = {};
+    if (this.isControlledComponent()) {
+      conditionalInputProps.checked = this.props.checked;
+    } else {
+      conditionalInputProps.onChange = this.onChange;
+    }
+    if (this.props.hasOwnProperty('defaultChecked')) {
+      conditionalInputProps.defaultChecked = this.props.defaultChecked;
+    }
+
     return (
-      <div className={styles.toggle} onClick={this.props.onClick}>
-        <input id={this.id} type="checkbox" className={styles.toggleCheckBox} />
+      <div className={styles.toggle} onClick={this.onClick}>
+        <input
+          id={this.id}
+          type="checkbox"
+          className={styles.toggleCheckBox}
+          {...conditionalInputProps}
+        />
         <label className={styles.toggleBtn} htmlFor={this.id}>
           <span className={styles.toggleFeature} />
         </label>
       </div>
     );
   }
+
+  private onClick = (e: React.SyntheticEvent<HTMLElement>) => {
+    const target = e.target as HTMLElement;
+    // This is triggered by the label and can be ignored as a duplicate
+    if (target.tagName === 'INPUT') {
+      return;
+    }
+
+    if (this.isControlledComponent() && this.props.onChange) {
+      this.props.onChange(!this.props.checked);
+    }
+  };
+
+  private onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!this.isControlledComponent() && this.props.onChange) {
+      this.props.onChange(e.target.checked);
+    }
+  };
+
+  private isControlledComponent = () => {
+    return this.props.hasOwnProperty('checked');
+  };
 }
--- stories/index.tsx
+++ stories/index.tsx
@@ -5,4 +5,29 @@ import { storiesOf } from '@storybook/react';
 
 import SmileyToggle from '../src/SmileyToggle';
 
-storiesOf('Button', module).add('with text', () => <SmileyToggle />);
+interface IControlledToggleState {
+  checked: boolean;
+}
+
+class ControlledToggle extends React.PureComponent<
+  any,
+  IControlledToggleState
+> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      checked: true,
+    };
+  }
+  public render() {
+    return (
+      <SmileyToggle onChange={this.onChange} checked={this.state.checked} />
+    );
+  }
+
+  private onChange = (next: boolean) => this.setState({ checked: next });
+}
+
+storiesOf('Button', module)
+  .add('uncontrolled', () => <SmileyToggle defaultChecked={true} />)
+  .add('controlled', () => <ControlledToggle />);
